f6a0815935a0be251e2881b98783c4b02ccb299b
bunkerWHz <aparsegov@gmail.com>
2019-10-14 19:51:44 +0300

    add slider functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-const slider = document.querySelector(".my-slider");
-const slides = [...document.querySelectorAll(".slider__item")];
+const slider = new Slider(".my-slider");
 
-const toggleActive = (el) => {
-el.classList.toggle("slider__item--active");
+
+class Slider {
+constructor(selector) {
+    this.selector = selector;
+    this.slider = document.querySelector(this.selector);
+    this.slides =  [...this.slider.querySelectorAll(".slide")];
+    this.current = this.slides.indexOf(this.slider.querySelector(".slide--active"));
+    this.sliderBtnLeft = this.slider.querySelector(".slider__btn--left").addEventListener("click", this.prevSlide.bind(this));
+    this.sliderBtnRight = this.slider.querySelector(".slider__btn--right").addEventListener("click", this.nextSlide.bind(this));
 }
-setInterval(toggleActive(slides[0], 2000));
+prevSlide() {
+    this.slides[this.current].classList.toggle('slide--active');
+    this.current--;
+    this.current === -1 ? this.current = this.slides.length -1 : 0;
+   this.slides[this.current].classList.toggle('slide--active');
+};
+nextSlide() {
+    this.slides[this.current].classList.toggle('slide--active');
+    this.current++;
+    this.current = this.current % this.slides.length;
+    this.slides[this.current].classList.toggle('slide--active');
+};
+}
